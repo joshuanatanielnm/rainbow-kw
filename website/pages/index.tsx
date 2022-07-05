9f0f4beca02f48911ba2dd11cda5b9d29743f4c2
Joshua Nathaniel M <clsjoshua@gmail.com>
2022-07-05 22:07:44 +0700

    feat: filter data in getStaticProps

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -15,8 +15,12 @@ export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch(ENDPOINT);
   const json = (await response.json()) as Rainbow;
 
+  const data = json.results.filter((val) => {
+    return val.token_id !== "0";
+  });
+
   return {
-    props: { data: json.results },
+    props: { data },
   };
 };
 
@@ -31,38 +35,32 @@ export default function HomePage({ data }: HomePageProps) {
         <Flex flexWrap="wrap" gap={6} justifyContent="space-around">
           {data.map((rainbow) => {
             return (
-              rainbow.token_id !== "0" && (
-                <Box key={rainbow.token_id}>
-                  <Link
-                    as={`/nft/${rainbow.asset_contract.address}_${rainbow.token_id}`}
-                    href="/nft/[address]"
-                    passHref
+              <Box key={rainbow.token_id}>
+                <Link as={`/nft/${rainbow.asset_contract.address}_${rainbow.token_id}`} href="/nft/[address]" passHref>
+                  <Box
+                    _hover={{
+                      transform: "scale(1.05)",
+                      transition: "0.2s",
+                    }}
+                    boxSize={{ base: "90vw", md: "45vw", lg: "30vw", xl: "13vw" }}
+                    cursor="pointer"
+                    overflow="hidden"
+                    rounded="2xl"
+                    shadow="2xl"
                   >
-                    <Box
-                      _hover={{
-                        transform: "scale(1.05)",
-                        transition: "0.2s",
-                      }}
-                      boxSize={{ base: "90vw", md: "45vw", lg: "30vw", xl: "13vw" }}
-                      cursor="pointer"
-                      overflow="hidden"
-                      rounded="2xl"
-                      shadow="2xl"
-                    >
-                      <Image
-                        alt={rainbow.metadata.name ?? rainbow.token_id}
-                        h="full"
-                        objectFit="cover"
-                        src={rainbow.metadata.image_url}
-                        w="full"
-                      />
-                    </Box>
-                  </Link>
-                  <Text color="gray.400" fontSize="sm" fontWeight="bold" pl="2" pt="1">
-                    {rainbow.metadata.name ?? rainbow.token_id}
-                  </Text>
-                </Box>
-              )
+                    <Image
+                      alt={rainbow.metadata.name ?? rainbow.token_id}
+                      h="full"
+                      objectFit="cover"
+                      src={rainbow.metadata.image_url}
+                      w="full"
+                    />
+                  </Box>
+                </Link>
+                <Text color="gray.400" fontSize="sm" fontWeight="bold" pl="2" pt="1">
+                  {rainbow.metadata.name ?? rainbow.token_id}
+                </Text>
+              </Box>
             );
           })}
         </Flex>
